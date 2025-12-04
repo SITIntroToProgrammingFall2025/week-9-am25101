60a56459982f0743f4cbaf93c07fc2a9fe7eeabe
AM25101-Mishti-Asher <am25101@shibaura-it.ac.jp>
2025-12-04 16:34:23 +0900

    Add sorting functionality for an array of numbers
    Implement a C program to sort an array of 5 numbers.

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[5];
+    int i, j, temp;
+
+    printf("Enter 5 numbers:\n");
+
+    for(i = 0; i < 5; i++){
+        scanf("%d", &a[i]);
+    }
+    for(i = 0; i < 5; i++){
+        for(j = i + 1; j < 5; j++){
+            if(a[i] > a[j]){
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    printf("Sorted array (small to large):\n");
+    for(i = 0; i < 5; i++){
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
+
 
