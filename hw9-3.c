d534af71ae8e4b5ebafb795184d23e44d31a8482
AM25101-Mishti-Asher <am25101@shibaura-it.ac.jp>
2025-12-05 12:36:41 +0900

    Update hw9-3.c

--- hw9-3.c
+++ hw9-3.c
@@ -2,33 +2,33 @@
 
 int main()
 {
-    int a[2][3], b[3][2], c[2][2];
+    double a[2][3], b[3][2], c[2][2];
     int i, j, k;
 
     //printf("Enter the elements of matrix A \n");
     for(i = 0; i < 2; i++){
         for(j = 0; j < 3; j++){
-            scanf("%d", &a[i][j]);
+            scanf("%lf", &a[i][j]);
         }
     }
 
     //printf("Enter the elements of matrix B (3x2):\n");
     for(i = 0; i < 3; i++){
         for(j = 0; j < 2; j++){
-            scanf("%d", &b[i][j]);
+            scanf("%lf", &b[i][j]);
         }
     }
     printf("The first matrix you entered is\n");
     for(i = 0; i < 2; i++){
         for(j = 0; j < 3; j++){
-            printf("%d ", a[i][j]);
+            printf("%.1f ", a[i][j]);
         }
         printf("\n");
     }
     printf("The second matrix you entered is\n");
     for(i = 0; i < 3; i++){
         for(j = 0; j < 2; j++){
-            printf("%d ", b[i][j]);
+            printf("%.lf ", b[i][j]);
         }
         printf("\n");
     }
@@ -45,12 +45,13 @@ int main()
     printf("The multiplication product of matrix A and matrix B:\n");
     for(i = 0; i < 2; i++){
         for(j = 0; j < 2; j++){
-            printf("%d ", c[i][j]);
+            printf("%.1f ", c[i][j]);
         }
         printf("\n");
     }
 	return 0;
 }
+
 
 
 
